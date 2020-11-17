3408bd7254471175067c3099d3a47423259e15cb
callanbr <chrisallan1992@icloud.com>
2020-11-17 16:50:08 -0700

    Raycast to shoot enemy

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -10,6 +11,7 @@ public class EnemyAI : MonoBehaviour {
 
   NavMeshAgent navMeshAgent;
   float distanceToTarget = Mathf.Infinity;
+  bool isProvoked = false;
 
   void OnDrawGizmosSelected() {
     Gizmos.color = new Color(1, 0, 0, 0.8F);
@@ -23,8 +25,27 @@ public class EnemyAI : MonoBehaviour {
 
   void Update() {
     distanceToTarget = Vector3.Distance(target.position, transform.position);
-    if (distanceToTarget <= chaseRange) {
-      navMeshAgent.SetDestination(target.position);
+    if (isProvoked) {
+      EngageTarget();
+    } else if (distanceToTarget <= chaseRange) {
+      isProvoked = true;
     }
+  }
+
+  private void EngageTarget() {
+    if (distanceToTarget >= navMeshAgent.stoppingDistance) {
+      ChaseTarget();
+    }
+    if (distanceToTarget <= navMeshAgent.stoppingDistance) {
+      AttackTarget();
+    }
+  }
+
+  private void ChaseTarget() {
+    navMeshAgent.SetDestination(target.position);
+  }
+
+  private void AttackTarget() {
+    print(name + " is attacking!");
   }
 }
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+
+  public Camera FPCamera;
+  public float range = 100f;
+
+  void Update() {
+    if (Input.GetButtonDown("Fire1")) {
+      Shoot();
+    }
+  }
+
+  private void Shoot() {
+    RaycastHit hit;
+    Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range);
+    print("HIT: " + hit.transform.name);
+  }
+}
