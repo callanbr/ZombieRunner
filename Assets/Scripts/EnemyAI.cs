6f1ff68cc3fcd5314eb98b2649374fe7af108c4a
callanbr <chrisallan1992@icloud.com>
2020-11-16 17:33:20 -0700

    first commit

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour {
+
+  public Transform target;
+  NavMeshAgent navMeshAgent;
+
+  void Start() {
+    navMeshAgent = GetComponent<NavMeshAgent>();
+  }
+
+  void Update() {
+    navMeshAgent.SetDestination(target.position);
+  }
+}
