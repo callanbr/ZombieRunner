31d880645d7da74605e98c30d1c78b3bb2ff1bde
callanbr <chrisallan1992@icloud.com>
2020-11-17 15:23:07 -0700

    enemy targer radius

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -6,13 +6,25 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour {
 
   public Transform target;
+  public float chaseRange = 5f;
+
   NavMeshAgent navMeshAgent;
+  float distanceToTarget = Mathf.Infinity;
+
+  void OnDrawGizmosSelected() {
+    Gizmos.color = new Color(1, 0, 0, 0.8F);
+    // Gizmos.DrawSphere(transform.position, chaseRange);
+    Gizmos.DrawWireSphere(transform.position, chaseRange);
+  }
 
   void Start() {
     navMeshAgent = GetComponent<NavMeshAgent>();
   }
 
   void Update() {
-    navMeshAgent.SetDestination(target.position);
+    distanceToTarget = Vector3.Distance(target.position, transform.position);
+    if (distanceToTarget <= chaseRange) {
+      navMeshAgent.SetDestination(target.position);
+    }
   }
 }
