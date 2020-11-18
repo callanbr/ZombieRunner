c62f14bcd8d9e605c5502e03f3dffdc906d19489
callanbr <chrisallan1992@icloud.com>
2020-11-17 20:06:04 -0700

    object hit VFX

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -9,6 +9,7 @@ public class Weapon : MonoBehaviour {
   [SerializeField] float range = 100f;
   [SerializeField] float damage = 30f;
   [SerializeField] ParticleSystem muzzleFlash;
+  [SerializeField] GameObject hitEffect;
 
   void Update() {
     if (Input.GetButtonDown("Fire1")) {
@@ -29,12 +30,17 @@ public class Weapon : MonoBehaviour {
   private void ProcessRaycast() {
     RaycastHit hit;
     if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range)) {
-      print("HIT: " + hit.transform.name);
+      CreateHitImpact(hit);
       EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
       if (target == null) { return; }
       target.TakeDamage(damage);
     } else {
       return;
     }
+  }
+
+  private void CreateHitImpact(RaycastHit hit) {
+    GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+    Destroy(impact, 1);
   }
 }
