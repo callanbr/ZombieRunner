757d810b52e2253afa6d87e3c32bbe768610c076
callanbr <chrisallan1992@icloud.com>
2020-11-19 21:06:20 -0700

    added attack amimaion placeholder

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -42,10 +42,13 @@ public class EnemyAI : MonoBehaviour {
   }
 
   private void ChaseTarget() {
+    GetComponent<Animator>().SetBool("attack", false);
+    GetComponent<Animator>().SetTrigger("move");
     navMeshAgent.SetDestination(target.position);
   }
 
   private void AttackTarget() {
+    GetComponent<Animator>().SetBool("attack", true);
     print(name + " is attacking!");
   }
 }
