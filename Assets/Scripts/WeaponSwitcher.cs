2c5bcb2f1716226f86bf7de8bd0867073759a414
callanbr <chrisallan1992@icloud.com>
2020-11-22 13:17:18 -0700

    changed scroll direction - Disabled for now

--- Assets/Scripts/WeaponSwitcher.cs
+++ Assets/Scripts/WeaponSwitcher.cs
@@ -51,14 +51,14 @@ public class WeaponSwitcher : MonoBehaviour {
   }
 
   private void ProcessScrollWheel() {
-    if (Input.GetAxis("Mouse ScrollWheel") > 0) {
+    if (Input.GetAxis("Mouse ScrollWheel") < 0) {
       if (currentWeapon >= transform.childCount - 1) {
         currentWeapon = 0;
       } else {
         currentWeapon++;
       }
     }
-    if (Input.GetAxis("Mouse ScrollWheel") < 0) {
+    if (Input.GetAxis("Mouse ScrollWheel") > 0) {
       if (currentWeapon <= 0) {
         currentWeapon = transform.childCount - 1;
       } else {
