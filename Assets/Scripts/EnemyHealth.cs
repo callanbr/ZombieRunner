6f931170c0351965c1e7b88c7e8d69ea7643f334
callanbr <chrisallan1992@icloud.com>
2020-11-22 17:14:31 -0700

    added zombie prefab - falls back on death

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,15 +1,28 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour {
   [SerializeField] float hitPoints = 100f;
 
+  bool isDead = false;
+
+  public bool IsDead() {
+    return isDead;
+  }
+
   public void TakeDamage(float damage) {
     BroadcastMessage("OnDamageTaken");
     hitPoints -= damage;
     if (hitPoints <= 0) {
-      Destroy(gameObject);
+      Death();
     }
+  }
+
+  private void Death() {
+    if (isDead) { return; }
+    isDead = true;
+    GetComponent<Animator>().SetTrigger("death");
   }
 }
